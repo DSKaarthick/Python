7c821e201083e8cc83c7e0650f347675ebd7a387
Kaarthick <63085392+DSKaarthick@users.noreply.github.com>
2020-04-14 16:23:11 +0530

    Add files via upload

--- Python-List.py
+++ Python-List.py
@@ -1,54 +1,153 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# # Understanding Basic Data Types
+# #  List
 
-# In[3]:
+# In[22]:
 
 
-a=10
-print(a)
-print(type(a))
+lst1 = [90,20,30,70,80]
+print(lst1)
+print(type(lst1))
 
 
-# In[4]:
+# In[23]:
 
 
-b=15.78
-print(b)
-print(type(b))
+lst2=[20,'Kaarthik',True,60]
+print(lst2)
+print(type(lst2))
 
 
-# In[5]:
+# # Access List elements by slicing
+
+# In[24]:
 
 
-c="Kaarthick"
-print(c)
-print(type(c))
+#Accessing the List Elements
+print(lst1[0])
+print(lst1[0:3])
+print(lst1[:2])
+print(lst1[0:])
 
 
-# In[6]:
+# In[25]:
 
 
-d=False 
-print(d)
-print(type(d))
+lst1.append(100)
+print(lst1)
+print(len(lst1))
 
 
-# In[7]:
+# In[26]:
 
 
-d=2+4j
-print(d)
-print(type(d))
+lst1.extend([120,150])
+print(lst1)
+print(len(lst1))
 
 
-# In[8]:
+# In[49]:
 
 
-f = 79.123456789012345678
-print(f)
-print(type(f))
+lst1.pop(1) # remove elements based on index 
+print(lst1)
+lst1.remove(150)
+print(lst1)
+
+
+# In[28]:
+
+
+print(lst1)
+
+
+# In[55]:
+
+
+lst2=[100,'Kaarthik',200]
+print(lst2)
+lst2.insert(300,3)
+print(lst2)
+
+
+# In[31]:
+
+
+lst3=[lst1,40,lst2]
+print(lst3)
+
+
+# In[37]:
+
+
+#Sorting the list elements
+lst1.sort()
+print(lst1)
+
+
+# In[38]:
+
+
+#Reverse the List Elements
+lst1.reverse()
+print(lst1)
+
+
+# In[45]:
+
+
+lst2=[100,'Kaarthik',200,300,400]
+print(lst2)
+#Deleting one element
+del lst2[1]
+print(lst2)
+#deleting multiple elements
+del lst2[1:2]
+
+
+# In[46]:
+
+
+print(lst2)
+
+
+# In[47]:
+
+
+#deleting entire list elements
+del lst2
+
+
+# # List Comprehension
+
+# In[52]:
+
+
+pow2=[]
+for i in range(10):
+    pow2.append(i**2)
+ 
+
+
+# In[53]:
+
+
+print(pow2)
+
+
+# In[56]:
+
+
+lst2=[3,4,5]
+MulpliedLst2=[i*3 for i in lst2]
+print(MulpliedLst2)
+
+
+# In[58]:
+
+
+low=['This','Is','Karthik']
+newlow=[i[0] for i in low]
+print(newlow)
 
 
 # In[ ]:
