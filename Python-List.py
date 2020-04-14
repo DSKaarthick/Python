897f2ea564b92d3811915f1586dc42d0457d85f2
Kaarthick <63085392+DSKaarthick@users.noreply.github.com>
2020-04-14 16:17:29 +0530

    Add files via upload

---
+++ Python-List.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Understanding Basic Data Types
+
+# In[3]:
+
+
+a=10
+print(a)
+print(type(a))
+
+
+# In[4]:
+
+
+b=15.78
+print(b)
+print(type(b))
+
+
+# In[5]:
+
+
+c="Kaarthick"
+print(c)
+print(type(c))
+
+
+# In[6]:
+
+
+d=False 
+print(d)
+print(type(d))
+
+
+# In[7]:
+
+
+d=2+4j
+print(d)
+print(type(d))
+
+
+# In[8]:
+
+
+f = 79.123456789012345678
+print(f)
+print(type(f))
+
+
+# In[ ]:
+
+
+
+
